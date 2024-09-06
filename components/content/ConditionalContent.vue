c4ba46f5103b52e0c869e68185b5a8755fa2f8ea
Matt Eason <matt.eason@fivium.co.uk>
2024-09-06 11:59:13 +0100

    add test conditional content component

---
+++ components/content/ConditionalContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps({
+  customerTypes: String
+})
+
+let customerType: string | null = null
+
+const expandedCustomerTypes = computed(() => {
+  if(props.customerTypes) {
+    return props.customerTypes.split(',')
+  }
+})
+
+const conditionMet = computed(() => {
+  if(expandedCustomerTypes.value && customerType) {
+    return expandedCustomerTypes.value.includes(customerType)
+  }
+})
+
+onMounted(() => {
+  const urlParams = new URLSearchParams(window.location.search)
+  customerType = urlParams.get('customerType')
+})
+</script>
+<template>
+  <template>
+    <slot/>
+  </template>
+</template>
